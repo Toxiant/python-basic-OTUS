02c68b39ca5493955fd5bf21b81f5e63d75ca730
Toxiant <Toxiant.toxianton@gmail.com>
2024-11-05 21:58:51 +0300

    Initial commit: Edited find and update

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,56 +1,32 @@
 import json
-from base64 import encode
-from logging import NullHandler
-from operator import iconcat, index
-from re import search
+
 
 def parse_phonebook(phonebook):
-        print('Name: ' + phonebook['name'])
-        print('phone number: ' + phonebook['phone_number'])
-        print('id: ' + phonebook['id'])
-        print(' ')
-
+    print('Name: ' + phonebook['name'])
+    print('phone number: ' + phonebook['phone_number'])
+    print('id: ' + phonebook['id'])
+    print(' ')
 
 def load_phonebook():
     '''Get all contacts'''
     with open('phonebook.json', 'r') as file:
-            phonebook = json.load(file)
-            for i in phonebook:
-                parse_phonebook(i)
-
+        phonebook = json.load(file)
+        for i in phonebook:
+            parse_phonebook(i)
 
 def add_contact(**kwargs):
     '''Add new contact'''
     with open('phonebook.json') as file:
-            contact = json.load(file)
-            new_contact = {
-                "name": input("Enter name - "),
-                "phone_number": input("Enter phone number - "),
-                "id": str(len(contact) + 1)
-            }
-            contact.append(new_contact)
+        contact = json.load(file)
+        new_contact = {
+            "name": input("Enter name - "),
+            "phone_number": input("Enter phone number - "),
+            "id": str(len(contact) + 1)
+        }
+        contact.append(new_contact)
     with open('phonebook.json', "w", encoding="utf-8") as file:
-         json.dump(contact, file, indent=4)
+        json.dump(contact, file, indent=4)
 
-# def find_contact(**kwargs):
-#     '''Find existing contacts'''
-#     print_contact = []
-#     null = []
-#     kwargs = str(input('Enter data - '))
-#     with open("phonebook.json", "r", encoding="utf-8") as file:
-#         contact = json.load(file)
-#         for i in contact:
-#             if i["name"].title() == kwargs.title():
-#                 print_contact.append(i)
-#             elif i["phone_number"] == kwargs:
-#                 print_contact.append(i)
-#             elif i["id"] == kwargs:
-#                 print_contact.append(i)
-#     if print_contact == null:
-#         print("The contact isn't exist")
-#     else:
-#         for i in print_contact:
-#             parse_phonebook(i)
 
 def find_contact(**kwargs):
     '''Find existing contacts'''
@@ -67,80 +43,101 @@ def find_contact(**kwargs):
             elif i["id"] == kwargs:
                 print_contact.append(i)
     if print_contact == null:
-        print("The contact isn't exist")
-    else:
-        return print_contact
+        print_contact = "Contact doesn't exist"
+
+    return print_contact
+
 
 def delete_contact(**kwargs):
     '''Delete existing contacts'''
-    kwargs = str(input('Enter data - '))
-    with open("phonebook.json", "r", encoding="utf-8") as file:
-        contact = json.load(file)
-        print(contact)
-        for i in contact:
-            if i["name"].title() == kwargs.title():
-                rm = contact.index(i)
-                contact.pop(rm)
-            elif i["phone_number"] == kwargs:
-                rm = contact.index(i)
-                contact.pop(rm)
-            elif i["id"] == kwargs:
-                rm = contact.index(i)
-                contact.pop(rm)
+    test = find_contact()
+    if test == "Contact doesn't exist":
+        print(test)
+    else:
+        if len(test) == 1:
+#   kwargs = str(input('Enter data - '))
+            with open("phonebook.json", "r", encoding="utf-8") as file:
+                contact = json.load(file)
+                for i in test:
+                    id_delete_contact = int(i["id"]) - 1
+                contact.pop(id_delete_contact)
         else:
-            print("The contact isn't exist")
-    for a in contact:
-        a["id"] = str(contact.index(a) +1 )
-    with open('phonebook.json', "w", encoding="utf-8") as file:
-        json.dump(contact, file, indent=4)
+            for i in test: parse_phonebook(i)
+            id_delete_contact = int(input("Find several contacts, choose ID contact for delete - ")) - 1
+            with open("phonebook.json", "r", encoding="utf-8") as file:
+                contact = json.load(file)
+            contact.pop(id_delete_contact)
+        for a in contact:
+            a["id"] = str(contact.index(a) +1 )
+        with open('phonebook.json', "w", encoding="utf-8") as file:
+            json.dump(contact, file, indent=4)
+
 
 def update_contact(**kwargs):
-    test = find_contact() # -переменной присваиваем найденный контакт (функия возвращает словарь)
-    id_update_contact = int(test["id"]) - 1
-    parse_phonebook(test)
-    with open("phonebook.json", "r", encoding="utf-8") as file:
-        edit_contact = json.load(file)
-        edit_contact[id_update_contact].update({
-            "name": input("Enter name - "),
-            "phone_number": input("Enter phone number - ")
-        })# -обращаемся к элементу в листе по id
-    # -изменяем найденный элемен
-    # -данные записываем в файл
-    with open('phonebook.json', "w", encoding="utf-8") as file:
-        json.dump(edit_contact, file, indent=4)
+    '''Edit existing contacts'''
+    test = find_contact()
+    if test == "Contact doesn't exist":
+        print(test)
+    else:
+        if len(test) == 1:
+            id_update_contact = int(test["id"]) - 1
+            parse_phonebook(test)
+            with open("phonebook.json", "r", encoding="utf-8") as file:
+                edit_contact = json.load(file)
+                edit_contact[id_update_contact].update({
+                    "name": input("Enter name - "),
+                    "phone_number": input("Enter phone number - ")
+                })
+            with open('phonebook.json', "w", encoding="utf-8") as file:
+                json.dump(edit_contact, file, indent=4)
+        if len(test) > 1:
+            for i in test: parse_phonebook(i)
+            id_update_contact = int(input("Find several contacts, choose ID contact for edit - ")) - 1
+            with open("phonebook.json", "r", encoding="utf-8") as file:
+                edit_contact = json.load(file)
+                edit_contact[id_update_contact].update({
+                    "name": input("Enter name - "),
+                    "phone_number": input("Enter phone number - ")
+                })
+            with open('phonebook.json', "w", encoding="utf-8") as file:
+                json.dump(edit_contact, file, indent=4)
 
-def menu(option):
+def start(command):
 
-    if option == "1":
+    if command == "1":
         load_phonebook()
 
-    if option == "2":
+    if command == "2":
         add_contact()
 
-    if option== "3":
-        find_contact()
+    if command == "3":
+        test = find_contact()
+        if test == "Contact doesn't exist":
+            print(test + "\n")
+        else:
+            for i in test:
+                parse_phonebook(i)
 
-
-    if option == "4":
+    if command == "4":
         update_contact()
-        print("Данные изменены")
+        print("Edited successfully")
 
-    if option == "5":
+    if command == "5":
         delete_contact()
-        print("Контакт удалён")
+        print("Deleted successfully")
 
 
 
 if __name__ == "__main__":
     while True:
-        menu(
+        start(
             input(
-                "Команды: \n "
-                "Показать все контакты - 1 \n "
-                "Создать контакт - 2 \n "
-                "Найти контакт - 3 \n "
-                "Изменить контакт - 4 \n"
-                "Удалить контакт - 5 \n"
-                " Введите команду -"
+                "Contacts: \n "
+                "Show all contacts - 1 \n "
+                "Create contact - 2 \n "
+                "Find contact - 3 \n "
+                "Edit contact - 4 \n"
+                "Delete contact - 5 \n"
+                "EEnter option number - "
             )
         )
